beea592f46a303399d78316f2de690c6d823fc5e
rajeev921 <rajeev.cse.imps@gmail.com>
2020-03-17 15:02:49 +0100

    Adding all the changes

--- LeetCode/Facebook/Arrays/238.cpp
+++ LeetCode/Facebook/Arrays/238.cpp
@@ -1,6 +1,27 @@
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
+        int N = nums.size();
+        vector<int>products(N, 0);
+        int p=1;
+        for(int i=0;i<N;++i) {
+          products[i]=p;
+          p*=nums[i];
+        }
+
+        p=1;
+        for(int i=N-1;i>=0;--i) {
+          products[i]*=p;
+          p*=nums[i];
+        }
+        
+        return products;
+    }
+}; 
+
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
         int sz = nums.size();
         if(sz <  3){
             if(sz < 1)
--- LeetCode/Facebook/Arrays/896.cpp
+++ LeetCode/Facebook/Arrays/896.cpp
@@ -52,3 +52,41 @@ public:
         return true;
     }
 };
+
+
+class Solution {
+public:
+    bool isMonotonic(vector<int>& A) {
+        int sz = A.size();
+        if(sz < 2) {
+            return true;
+        }
+        
+        int first = A[0];
+        int i{1};
+        for(; i < sz; ++i) {
+            int second = A[i];
+            if(first > second) {
+                break;
+            }
+            first = second;
+        }
+        if(i==sz){
+            return true;
+        }
+        
+        i=1;
+        first= A[0];
+        for(; i < sz; ++i) {
+            int second = A[i];
+            if(first < second) {
+                break;
+            }
+            first = second;
+        }
+        if(i==sz){
+            return true;
+        }
+        return false;
+    }
+};
